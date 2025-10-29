16964873ccccea7988a96d649b3c7daf22ae8344
수현 <esuhyun0416@gmail.com>
2025-10-29 14:27:48 +0900

    Add frontend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import GuidePage from "./pages/GuidePage";
+import Payment from "./pages/Payment";
+import Admin from "./pages/Admin";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/GuidePage" element={<GuidePage />} />
+        <Route path="/Payment" element={<Payment />} />
+        <Route path="/Admin" element={<Admin />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
